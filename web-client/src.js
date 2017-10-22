2b02af363fede6d6002d9ff6107c788ce89d0732
Adam Simonini <adam.simonini@gmail.com>
2017-10-22 08:54:22 -0400

    Updates

--- web-client/src.js
+++ web-client/src.js
@@ -1,48 +1,58 @@
+$(document).ready(function(){
 
-// A time function, counting up from 0
-var sec = 0;
-  function pad ( val ) { return val > 9 ? val : "0" + val; }
-  setInterval( function(){
-      $(".seconds").html(pad(++sec%60));
-      $(".minutes").html(pad(parseInt(sec/60,10)));
-  }, 1000);
+  // A time function, counting up from 0
+  var sec = 0;
+    function pad ( val ) { return val > 9 ? val : "0" + val; }
+    setInterval( function(){
+        $(".seconds").html(pad(++sec%60));
+        $(".minutes").html(pad(parseInt(sec/60,10)));
+    }, 1000);
 
-// Split name into two values
-    let focus = 0;
-    let rawName = $("#inputName");
-    //if first time inputting name, check if more than one set of characters is input
-    rawName.focusout(function(){
-      let nameArray = rawName.val().split(" ");
-      if (nameArray.length < 2 && focus == 0){
-        alert("Please input both a first & last name.");
-        focus++;
-        return false;
+    //Upon submit, transfer data over to base level
+
+      function newPatient(patient){
+        alert(patient);
       }
-      let first = nameArray[0]
-      nameArray.shift()
-      let last = nameArray.toString().replace(",", " ");
 
-  //Upon submit, transfer data over to base level
+      function getLastName(){
+        let rawName = $("#inputName");
+        //if first time inputting name, check if more than one set of characters is input
+        let nameArray = rawName.val().split(" ");
+        if (nameArray.length < 2 && focus == 0){
+          alert("Please input both a first & last name.");
+          return false;
+        }
+        nameArray.shift()
+        let last = nameArray.toString().replace(",", " ");
+        return(last);
+      }
 
       $("#submitBtn").click(function(){
+        function generatePatient(){
+          let patient = [];
+          patient[0] = $("#inputPriority").val()
+          patient[1] = getLastName();
+          patient[2] = $("#inputGender").val()
+          patient[3] = $("#inputAge").val();
+          patient[4] = $("#inputConditions").val();
+          patient[5] = $("#inputSymptom").val();
+          patient[6] = $("#inputNotes").val();
 
-        var data = {
-            title: "Constructing HTML Elements"
+          alert($("#inputSymptom").closest().attr('id'));
+
+          return(patient);
         }
+        let patient = generatePatient();
 
-        var template = $("#tutorial-template").html();
-        // html: '<div ...>\n<h1 ...>{{title }}<h1>\n</div>'
+        function generatePatientDiv(){
 
-        var html = Mustache.render(template, data);
-        $("body").append(html);
-
-        $("#lastname").val(last);
-        $("#gender").val($("#inputGender").val());
-        $("#age").val($("#inputAge").val());
-        $("#conditions").val($("#inputConditions").val());
-        $("#notes").val($("#inputNotes").val());
+        }
+        generatePatientDiv(patient);
+        // $("body").append(newPatient(patient));
       });
-    });
+});
+
+
     // let radio = 1;
     // $("#radioButton").on("click", function(){
     //   if($(this).val) == radio){
