53ffcb773168b1b2382f6429f9ddb7ea226d76a7
William Wang <billwang115@gmail.com>
2017-10-21 22:50:25 -0400

    test

--- William/tests.py
+++ William/tests.py
@@ -1,9 +1,13 @@
-Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> word=5
->>> print word
-SyntaxError: Missing parentheses in call to 'print'. Did you mean print(word)?
->>> word=5
->>> print(word)
-5
->>> 
+name = input("What's your name? ")
+print("Nice to meet you " + name + "!")
+age = input("Your age? ")
+print("So, you are already " + str(age) + " years old, " + name + "!")
+
+
+
+
+
+
+
+
+
